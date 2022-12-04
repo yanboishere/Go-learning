9bf9fce5069d9fa968dff7c88be034fe87eef3f8
Yanbo Wang <yanbotravelaroundworld@gmail.com>
2022-12-04 12:39:06 +0800

    Change file by Yanbo Wang.

--- Command line prameter/command line.go
+++ Command line prameter/command line.go
@@ -15,8 +15,9 @@ func main() {
 		s += sep + os.Args[i]
 		/*
 			sep + os.Args[i] 表示将 sep 和 os.Args[i〕追加到一起
-				语句
-				S += sep + os.Args[i]
+
+			语句：
+			S += sep + os.Args[i]
 
 				是一个赋值语句，将sep 和os.Args[i]追加到旧的s上面，并且重新赋给 s，相当于语句：
 				s=s+sep + os.Args[i]
