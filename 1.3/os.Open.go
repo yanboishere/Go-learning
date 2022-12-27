45688550c878ba32f8843dcdab9882c65433b7ad
Yanbo Wang <yanbotravelaroundworld@gmail.com>
2022-12-27 21:16:43 +0800

    Changed file by Yanbo Wang with GoLand.

---
+++ 1.3/os.Open.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprint(os.Stderr, "dup2:%v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
